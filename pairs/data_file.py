6849a51964afa5c2e4a315411a08519f84109026
Adam Walsh <adamtwalsh@gmail.com>
2018-04-26 02:38:45 -0400

    update data_set, gitignore

---
+++ pairs/data_file.py
@@ -0,0 +1,2 @@
+class DataFile:
+    pass
--- pairs/data_set.py
+++ pairs/data_set.py
@@ -1,6 +1,7 @@
 import os
 import re
-from datetime import date
+from datetime import date, datetime
+from warnings import warn
 
 import pandas as pd
 from dateutil import relativedelta
@@ -24,28 +25,22 @@ class DataSet(object):
     assert len(nasdaq_years) == len(nyse_years) and sorted(nasdaq_years) == sorted(
         nyse_years), 'Exchange year data directories do not match.'
 
-    # Get that most recent day for the most current year
-    # List of days in NASDAQ set for most recent year
-    # nasdaq_days_set = [c for a, b, c in os.walk(nasdaq_dir + '/{}'.format(max(nasdaq_years)))][0]
-    # nasdaq_most_recent_day = max(list(map(int, [re.split('_|\.', d)[1] for d in nasdaq_days_set])))
-    # List of days in NYSE set for most recent year
-    # nyse_days_set = [c for a, b, c in os.walk(nyse_dir + '/{}'.format(max(nyse_years)))][0]
-    # nyse_most_recent_day = max(list(map(int, [re.split('_|\.', d)[1] for d in nyse_days_set])))
-    # Make sure the days are the same
-    # assert nasdaq_most_recent_day == nyse_most_recent_day, 'Exchange most recent days do not match.'
-
-    def __init__(self, nyse: bool = True, nasdaq: bool = True, months: int = 12, year: int = 2017):
+    def __init__(self, months: int = 12, year: int = 2017):
         # Set the year
         if year and year not in list(set(self.nasdaq_years + self.nyse_years)):
             raise ValueError("Year is not in data set.")
         else:
             self.year = year
+        print('Preparing data for Y{}'.format(self.year))
 
         # Create lists of days for each exchange
-        self.nasdaq_day_files = [c for a, b, c in os.walk(nasdaq_dir + '/{}'.format(max(self.nasdaq_years)))][0]
-        self.nyse_day_files = [c for a, b, c in os.walk(nyse_dir + '/{}'.format(max(self.nyse_years)))][0]
+        # TODO: There should be a DataFile class for these.
+        self.nasdaq_day_files = [c for a, b, c in os.walk(nasdaq_dir + '/{}'.format(self.year))][0]
         self.nasdaq_days = list(map(int, [re.split('_|\.', d)[1] for d in self.nasdaq_day_files]))
+        self.nasdaq_dates = [datetime.strptime(str(d), '%Y%m%d') for d in self.nasdaq_days]
+        self.nyse_day_files = [c for a, b, c in os.walk(nyse_dir + '/{}'.format(self.year))][0]
         self.nyse_days = list(map(int, [re.split('_|\.', d)[1] for d in self.nyse_day_files]))
+        self.nyse_dates = [datetime.strptime(str(d), '%Y%m%d') for d in self.nyse_days]
 
         assert len(self.nasdaq_days) == len(self.nyse_days) and sorted(self.nasdaq_days) == sorted(
             self.nyse_days), 'Exchange day sets do not match'
@@ -54,21 +49,43 @@ class DataSet(object):
         self.expected_trading_days = pd.DatetimeIndex(start='{}-01-01'.format(self.year),
                                                       end='{}-12-31'.format(self.year), freq=self.us_bd).size
 
+        # Get number of trading days
         self.num_trading_days = len(list(set(self.nasdaq_days + self.nyse_days)))
-
-        # Read nyse data
-        if nyse:
-            pass
-        # Read nasdaq data
-        if nasdaq:
-            pass
-        #
-        assert 0 < months < 24, 'Parameter months must be in range: 0 < months < 24'
+        if self.expected_trading_days != self.num_trading_days:
+            warn('Number of days in data set ({}) does not match the number of expected trading days ({}).'.format(
+                self.num_trading_days, self.expected_trading_days))
 
         # TODO: user should be able to check for n months back in data set, if day n months ago is NOT a business day,
         #       get the next business day.
-        if not is_business_day(get_relative_date(months)):
-            pass
+        # TODO: should also be able to set a date range for data
+        # assert 0 < months < 24, 'Parameter months must be in range: 0 < months < 24'
+        # if not is_business_day(get_relative_date(months)):
+        #     pass
+
+        # Load data
+        self.data: pd.DataFrame = self.load_dataframe()
+
+    def load_dataframe(self) -> pd.DataFrame:
+        df = pd.DataFrame()
+        df.index.name = 'ticker'
+
+        # TODO: fix df has NaN values after reading in 2nd data set
+        # Read data files into DataFrame
+        self.df_read_data_sets('{}{}/'.format(nasdaq_dir, self.year), df)
+        # self.df_read_data_sets('{}{}/'.format(nyse_dir, self.year), df)
+
+        df_t = df.T
+
+        return df_t
+
+    @staticmethod
+    def df_read_data_sets(dir_path: str, main_df: pd.DataFrame):
+        # Columns of the CSV data files
+        data_cols = ["ticker", "date", "open", "high", "low", "close", "volume"]
+        # Read each record into the main DataFrame
+        for file in os.listdir(dir_path):
+            df = pd.read_csv(dir_path + file, header=None, names=data_cols, index_col=0)
+            main_df.loc[:, df.date.iloc[0]] = df.close
 
 
 def get_relative_date(months: int) -> date:
